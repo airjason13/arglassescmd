8c125cb319d51b32f95b5af4cf38e057e1ac0984
airjason13 <airjason13@gmail.com>
2025-09-26 10:18:54 +0800

    1. modified README.md         2. add convert_cmds_to_dicts.py         3. revision

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # arglassescmd
+
+Please add commands in cmd_def.py with corresponding format.
+Make sure the convert_cmds_to_dicts.py could generate cmd_dict.py correctly.
+
+    python3 convert_cmd
--- cmd_def.py
+++ cmd_def.py
@@ -1,8 +1,8 @@
 ''' CMD @20250919 '''
-CMD_Version_PN = "ARSYS"
+CMD_Version_PN = "ARGLASSES_CMD"
 CMD_Version_Year = '2025'
 CMD_Version_Month = '09'
-CMD_Version_Date = '24'
+CMD_Version_Date = '26'
 CMD_Version_Major = "01"
 CMD_Version_Minor = "00"
 CMD_Version_Patch = "00"
@@ -15,17 +15,32 @@ CMD_Version = CMD_Version_PN + "_" + \
           CMD_Version_Minor + \
           CMD_Version_Patch
 
-# SPECIAL CMD
+# SPEC_CMD
 MSG_SPEC_HELLO="msg_spec_hello"
+# SPEC_CMD_SYS_Reboot
+SYS_SPEC_GOING_TO_REBOOT="msg_spec_going_to_reboot"
 
-# GET CMD
+# GET_CMD_VERSION
 MSG_GET_SW_VERSION="msg_get_sw_version"
 LE_GET_SW_VERSION="le_get_sw_version"
 DEMO_GET_SW_VERSION="demo_get_sw_version"
 SYS_GET_SW_VERSION="sys_get_sw_version"
 
+# GET_CMD_SYS_Wifi
+SYS_GET_WIFI_UAP0_SSID="sys_get_wifi_uap0_ssid"
+SYS_GET_WIFI_UAP0_PWD="sys_get_wifi_uap0_pwd"
+SYS_GET_WIFI_UAP0_SSID_PWD="sys_get_wifi_uap0_ssid_pwd"
+SYS_GET_WIFI_UAP0_HW_MODE="sys_get_wifi_uap0_hw_mode"
 
-#SET CMD
+
+# SET_CMD
 DEMO_SET_PLAY_MEDIA="demo_set_play_media"
 SYS_SET_WIFI_SSID="sys_set_wifi_ssid"
 LE_SET_BRIGHTNESS="le_set_brightness"
+
+# SET_CMD_SYS_Wifi
+SYS_SET_WIFI_UAP0_SSID="sys_set_wifi_uap0_ssid"
+SYS_SET_WIFI_UAP0_PWD="sys_set_wifi_uap0_pwd"
+SYS_SET_WIFI_UAP0_SSID_PWD="sys_set_wifi_uap0_ssid_pwd"
+SYS_SET_WIFI_UAP0_HW_MODE="sys_s/et_wifi_uap0_hw_mode"
+
