112d16cc419aeff1facae1667f06407148baf016
airjason13 <airjason13@gmail.com>
2025-10-03 11:30:09 +0800

    1. refactor le set/get cmd         2. revision

--- cmd_def.py
+++ cmd_def.py
@@ -1,8 +1,8 @@
 ''' CMD @20250919 '''
 CMD_Version_PN = "ARGLASSES_CMD"
 CMD_Version_Year = '2025'
-CMD_Version_Month = '09'
-CMD_Version_Date = '26'
+CMD_Version_Month = '10'
+CMD_Version_Date = '03'
 CMD_Version_Major = "01"
 CMD_Version_Minor = "00"
 CMD_Version_Patch = "00"
@@ -15,10 +15,18 @@ CMD_Version = CMD_Version_PN + "_" + \
           CMD_Version_Minor + \
           CMD_Version_Patch
 
+
+''' ============== Start of SPEC CMD ============== '''
+
 # SPEC_CMD
 MSG_SPEC_HELLO="msg_spec_hello"
 # SPEC_CMD_SYS_Reboot
 SYS_SPEC_GOING_TO_REBOOT="msg_spec_going_to_reboot"
+
+
+''' ============== End of SPEC CMD ============== '''
+
+''' ============== Start of GET CMD ============== '''
 
 # GET_CMD_VERSION
 MSG_GET_SW_VERSION="msg_get_sw_version"
@@ -32,24 +40,29 @@ SYS_GET_WIFI_UAP0_PWD="sys_get_wifi_uap0_pwd"
 SYS_GET_WIFI_UAP0_SSID_PWD="sys_get_wifi_uap0_ssid_pwd"
 SYS_GET_WIFI_UAP0_HW_MODE="sys_get_wifi_uap0_hw_mode"
 
-
-# SET_CMD
-DEMO_SET_PLAY_MEDIA="demo_set_play_media"
-SYS_SET_WIFI_SSID="sys_set_wifi_ssid"
-LE_SET_BRIGHTNESS="le_set_brightness"
-LE_SET_CURRENT="le_set_current"
-LE_SET_TEMPERATURE="le_set_temperature"
-LE_SET_MIRROR="le_set_mirror"
-LE_SET_FLIP="le_set_flip"
-LE_SET_OFFSET="le_set_offset"
-
-# GET_CMD
+# GET_CMD_LE
 LE_GET_BRIGHTNESS="le_get_luminance"
 LE_GET_CURRENT="le_get_current"
 LE_GET_TEMPERATURE="le_get_temperature"
 LE_GET_MIRROR="le_get_mirror"
 LE_GET_FLIP="le_get_flip"
 LE_GET_OFFSET="le_get_offset"
+
+''' ============== End of GET CMD ================ '''
+
+''' ============== Start of SET CMD ============== '''
+
+# SET_CMD
+DEMO_SET_PLAY_MEDIA="demo_set_play_media"
+SYS_SET_WIFI_SSID="sys_set_wifi_ssid"
+
+# SET_CMD_LE 
+LE_SET_BRIGHTNESS="le_set_brightness"
+LE_SET_CURRENT="le_set_current"
+LE_SET_TEMPERATURE="le_set_temperature"
+LE_SET_MIRROR="le_set_mirror"
+LE_SET_FLIP="le_set_flip"
+LE_SET_OFFSET="le_set_offset"
 
 # SET_CMD TEST
 DEMO_SET_TEST="demo_set_test"
@@ -62,3 +75,4 @@ SYS_SET_WIFI_UAP0_PWD="sys_set_wifi_uap0_pwd"
 SYS_SET_WIFI_UAP0_SSID_PWD="sys_set_wifi_uap0_ssid_pwd"
 SYS_SET_WIFI_UAP0_HW_MODE="sys_s/et_wifi_uap0_hw_mode"
 
+''' ============== End of SET CMD ============== '''
